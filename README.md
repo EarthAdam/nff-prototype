0097e64974db1cb1f4789becd76650fe3a1810f4
Adam Sauer <adamsauer.ee@gmail.com>
2021-11-06 23:25:57 -0400

    First NFF

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # nff-prototype
 First stab at NFF
+
+![](final.gif)
+
+GIF Link: https://bit.ly/3bOaE8r
+OpenSeas Link: https://bit.ly/3mQZ9n3
+
+Used this tutorial: https://www.youtube.com/watch?v=K0fq8BMScEc
+1. pip install myqr
+1. python gif_QRCode.py
---
+++ gif_QRCode.py
@@ -0,0 +1,41 @@
+#Filename:- gif_QRCode.py
+#Required
+#pip install MyQR
+
+from MyQR import myqr
+import os
+  
+version, level, qr_name = myqr.run(
+  # Add string or a URL (add http(s):// before it)
+  words="https://bit.ly/3mQZ9n3",     
+
+  # Set the highest fault tolerance rate
+  version=1,               
+
+  # Control the error correction level,
+  # the range is L, M, Q, H, increasing from left to right
+  level='H',               
+
+  # Combining QR code + Image
+  #Add file name eg. your_image.gif
+  picture="nff1.gif", 
+
+  # Color QR code            
+  colorized=True,    
+
+  # Set contrast of the image,
+  # 1.0 - original picture / default,
+  # small value - low contrast,
+  # large value - high contrast.        
+  contrast=1.0,      
+
+  # Set brightness of the image,
+  # adjustment values ​​same as above        
+  brightness=1.0,       
+
+  # Save the file name, the format can be jpg, png, bmp, gif      
+  save_name="final.gif",
+
+  #Control location      
+  save_dir=os.getcwd()          
+)
